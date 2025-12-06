cb6f7c8445fab0c43f78aa4f90087a03b4353221
Amaaruu <isaac.gonza1210@gmail.com>
2025-12-06 01:24:35 -0300

    Detalle (Se arreglan comentarios)

--- src/main/java/com/example/OnlyKick/security/JwtUtil.java
+++ src/main/java/com/example/OnlyKick/security/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    // CAMBIO 1: Inyectamos el valor desde application.properties
+    //Inyectamos el valor desde application.properties
     @Value("${jwt.secret}")
     private String secretKey;
 
