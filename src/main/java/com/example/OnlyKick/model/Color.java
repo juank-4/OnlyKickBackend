036e4fbbe8f9f2a0424e57ff91f51d9124c2e93f
Amaaruu <isaac.gonza1210@gmail.com>
2025-12-07 19:49:42 -0300

    Detalle

--- src/main/java/com/example/OnlyKick/model/Color.java
+++ src/main/java/com/example/OnlyKick/model/Color.java
@@ -1,17 +1,21 @@
 package com.example.OnlyKick.model;
 
+import java.util.Set;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.Entity; 
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedValue;        
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import java.util.Set;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
@@ -33,9 +37,13 @@ public class Color {
     //Relaciones
     @OneToMany(mappedBy = "color", fetch = FetchType.LAZY)
     @JsonIgnore
+    @ToString.Exclude          
+    @EqualsAndHashCode.Exclude
     private Set<Inventario> inventario;
 
     @OneToMany(mappedBy = "color", fetch = FetchType.LAZY)
     @JsonIgnore
+    @ToString.Exclude        
+    @EqualsAndHashCode.Exclude
     private Set<ProductosVenta> productosVenta;
 }
